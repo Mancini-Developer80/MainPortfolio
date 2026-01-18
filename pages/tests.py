5d250a97c8bee85f35a2c5626d4e54e5253bd382
Giuseppe Mancini <giuseppe.mancini80@gmail.com>
2026-01-18 15:05:18 +0100

    Chore: Unificazione repository e pulizia storica file ignorati

--- pages/tests.py
+++ pages/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
 
-# Create your tests here.
+from django.test import TestCase
+from django.urls import reverse
+
+class PortfolioTests(TestCase):
+
+    def test_about_page_status_code(self):
+        # Sostituisci 'about' con il nome che hai nel file urls.py
+        response = self.client.get(reverse('pages:about'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_about_page_contains_architect_text(self):
+        # Questo verifica che il tuo nuovo brand sia presente nell'HTML
+        response = self.client.get(reverse('pages:about'))
+        self.assertContains(response, "AI Solution Architect in training")
+
+
